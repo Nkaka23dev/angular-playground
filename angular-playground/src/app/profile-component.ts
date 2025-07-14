6fdccb1462cb92a9307b0e87fe1791513fa242b2
ericnkaka <nkakaeric96@gmail.com>
2025-07-14 17:36:35 +0200

    feat: finishing bubble sort with generics

---
+++ angular-playground/src/app/profile-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+ standalone: true,
+ selector: 'app-profile, [app-profile]:not(p)',
+ templateUrl: './profile-component.html',
+ styleUrls: ['./profile-component.css']
+})
+export default class ProfileComponent {} 
+
+
